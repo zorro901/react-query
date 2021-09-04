5a6c62d62dd8bc0cdedbd626352ef58cc7fc1b4e
zorro901 <zoro907@gmail.com>
2021-09-05 03:03:08 +0900

    9. Classical server data state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React, { VFC } from 'react'
+import { VFC } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ClassicalFetchA } from './components/ClassicalFetchA'
+import { ClassicalFetchB } from './components/ClassicalFetchB'
 import { Layout } from './components/Layout'
+import { StateProvider } from './context/StateProvider'
 
 const App: VFC = () => {
   return (
     <BrowserRouter>
-      <Layout>
-        App
-      </Layout>
+      <StateProvider>
+        <Layout>
+          <Switch>
+            <Route exact path={'/fetch-a'}>
+              <ClassicalFetchA/>
+            </Route>
+            <Route exact path={'/fetch-b'}>
+              <ClassicalFetchB/>
+            </Route>
+          </Switch>
+        </Layout>
+      </StateProvider>
     </BrowserRouter>
   )
 }
