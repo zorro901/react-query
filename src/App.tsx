d0e3937d5547ec5fd08a12ceddac384e8055fde6
zorro901 <zoro907@gmail.com>
2021-09-05 03:32:15 +0900

    10. React-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import { VFC } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { ClassicalFetchA } from './components/ClassicalFetchA'
 import { ClassicalFetchB } from './components/ClassicalFetchB'
 import { Layout } from './components/Layout'
+import { ReactQueryA } from './components/ReactQueryA'
+import { ReactQueryB } from './components/ReactQueryB'
 import { StateProvider } from './context/StateProvider'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 const App: VFC = () => {
   return (
-    <BrowserRouter>
-      <StateProvider>
-        <Layout>
-          <Switch>
-            <Route exact path={'/fetch-a'}>
-              <ClassicalFetchA/>
-            </Route>
-            <Route exact path={'/fetch-b'}>
-              <ClassicalFetchB/>
-            </Route>
-          </Switch>
-        </Layout>
-      </StateProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <StateProvider>
+          <Layout>
+            <Switch>
+              <Route exact path={'/'}>
+                <ReactQueryA/>
+              </Route>
+              <Route exact path={'/query-b'}>
+                <ReactQueryB/>
+              </Route>
+              <Route exact path={'/fetch-a'}>
+                <ClassicalFetchA/>
+              </Route>
+              <Route exact path={'/fetch-b'}>
+                <ClassicalFetchB/>
+              </Route>
+            </Switch>
+          </Layout>
+        </StateProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
   )
 }
 
